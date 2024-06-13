50e4676584b43e4b9a088ec3a91aab7990750d5b
ghidav <davide.ghilardi0@gmail.com>
2024-06-13 12:57:31 +0000

    modified:   task_gen.py 	new file:   tasks/ioi/cfg.json 	deleted:    tasks/ioi/ioi.json 	deleted:    tasks/ioi/names.txt

--- task_gen.py
+++ task_gen.py
@@ -2,41 +2,64 @@ import pandas as pd
 import numpy as np
 from transformers import AutoTokenizer
 import numpy.random as random
+import argparse
+import json
 
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
-names = ["John", "Mary", "Paul", "Anna", "Mark", "Lucy", "David", "Emma", "James", "Sara", "Lisa", "Brian", "Eric", "Jane", "Peter", "Susan", "Chris", "Nina", "Helen", "Diane", "Alice", "Bruce", "Kevin", "Linda", "Laura", "Megan", "Ryan", "Julie", "Steve", "Aaron", "Molly", "Cindy", "Grace", "Mason", "Ethan", "Chloe", "Claire", "Olivia", "Henry", "Nancy", "Maria", "Gary", "Karen", "Betty", "Shawn", "Holly", "Amber", "Tracy", "Judy"]
+# Argument parser
+parser = argparse.ArgumentParser(description='Generate tasks for SAE transfer learning')
+parser.add_argument('-m', '--model', type=str, default='ioi', help='Model to generate tasks for.')
+parser.add_argument('-t', '--task', type=str, default='ioi', help='Task to generate.')
+args = parser.parse_args()
 
-names_tokens = [tokenizer(' '+n, return_tensors='pt')['input_ids'][0] for n in names]
-names = [n for n, t in zip(names, names_tokens) if len(t) == 1]
+tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+with open(f'tasks/{args.task.lower()}/cfg.json', 'r') as f:
+    base_cfg = json.load(f)
 
 # IOI
-def get_names():
-    io, s1 = random.choice(np.arange(len(names)), size=2, replace=False)
-    return names[io], names[s1]
+if args.tasks == 'ioi':
+    names = base_cfg['variables'][0]['values']
+    names_tokens = [tokenizer(' '+n, return_tensors='pt')['input_ids'][0] for n in names]
+    names = [n for n, t in zip(names, names_tokens) if len(t) == 1]
+    
+    templates = base_cfg['templates']
 
-abb_ioi_template = "When {IO} and {S1} went to the store, {S1} gave a drink to"
-bab_ioi_template = "When {S1} and {IO} went to the store, {S1} gave a drink to"
-
-prompts = []
-for i in range(len(names)):
-    for j in range(len(names)):
-        if i != j:
-            pos = random.rand()
-            if pos > 0.5:
-                ioi_template = abb_ioi_template
-            else:
-                ioi_template = bab_ioi_template
-            
-            io, s1 = names[i], names[j]
-            prompt = ioi_template.format(IO=io, S1=s1)
-            tokens = tokenizer(prompt, return_tensors='pt')['input_ids'][0]
-            str_tokens = tokenizer.convert_ids_to_tokens(tokens)
+    prompts = []
+    for i in range(len(names)):
+        for j in range(len(names)):
+            if i != j:
+                pos = random.rand()
+                if pos > 0.5:
+                    ioi_template = templates[0] # ABB
+                else:
+                    ioi_template = templates[1] # BAB
                 
-            prompts.append({
-                'prompt': prompt,
-                'io': ' '+names[i],
-                's1': ' '+names[j],
-                'pos': 'ABB' if pos > 0.5 else 'BAB'
-            })
+                io, s1 = names[i], names[j]
+                prompt = ioi_template.format(IO=io, S1=s1)
+                tokens = tokenizer(prompt, return_tensors='pt')['input_ids'][0]
+                str_tokens = tokenizer.convert_ids_to_tokens(tokens)
+                    
+                prompts.append({
+                    "prompt": prompt,
+                    "variables": [
+                        {
+                            "name": "IO",
+                            "value": names[i],
+                            "pos": 2 if pos > 0.5 else 4
+                        },
+                        {
+                            "name": "S1",
+                            "value": names[j],
+                            "pos": 4 if pos > 0.5 else 2
+                        },
+                        {
+                            "name": "S2",
+                            "value": names[j],
+                            "pos": 10
+                        }
+                ]})
 
-pd.DataFrame(prompts).to_json('tasks/ioi.json', orient='records')
+    base_cfg['prompts'] = prompts
+    # Write updated config
+    with open(f'tasks/{args.task.lower()}/task.json', 'w') as f:
+        json.dump(base_cfg, f, indent=4)
