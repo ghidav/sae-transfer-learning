e1da3337376453dc5d9cd26419962ed010b459d9
Davide Ghilardi <131039402+ghidav@users.noreply.github.com>
2024-08-05 11:59:11 +0200

    modified:   eval_analysis.ipynb 	modified:   sae_eval.py

--- sae_eval.py
+++ sae_eval.py
@@ -1,78 +1,152 @@
+from sae_lens.evals import run_evals, EvalConfig
+from sae_lens import SAE, ActivationsStore, HookedSAETransformer
+from sae_lens.config import LanguageModelSAERunnerConfig
+import argparse
 import torch
-import pandas as pd
-import numpy as np
-from sae_lens import LMSparseAutoencoderSessionloader
-from huggingface_hub import snapshot_download
-import os
 
-from transformer_lens import utils
-from functools import partial
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
 
-os.environ['HF_HOME'] = '/workspace/huggingface'
+# Parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("-sae", "--sae", type=str)
+parser.add_argument("-act", "--act", type=str)
+args = parser.parse_args()
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+sae_layer, sae_component = args.sae.split("c")
+sae_layer = int(sae_layer[1:])
 
-layer = 8 # pick a layer you want.
-REPO_ID = "ghidav/pythia-70m-deduped-sae"
-FILENAME = f"test/blocks.4.hook_resid_pre"
+act_layer, act_component = args.act.split("c")
+act_layer = int(act_layer[1:])
 
-path = snapshot_download(repo_id=REPO_ID)
-print(path)
-model, sparse_autoencoder, activation_store = LMSparseAutoencoderSessionloader.load_pretrained_sae(
-    path=os.path.join(path, FILENAME), device=device
+# SAE Config
+cfg = LanguageModelSAERunnerConfig(
+    
+    # Data Generating Function (Model + Training Distibuion)
+    model_name = "pythia-160m-deduped",
+    hook_name = f"blocks.{act_layer}.{act_component}",
+    hook_layer = act_layer,
+    dataset_path = "NeelNanda/pile-small-tokenized-2b",
+    is_dataset_tokenized = True,
+    context_size = 1024,
+    streaming=True,
+
+    # SAE Parameters
+    architecture = "jumprelu",
+    d_in = 768,
+    d_sae = None,
+    b_dec_init_method = "zeros",
+    expansion_factor = 8,
+    activation_fn = "relu",  # relu, tanh-relu, topk
+    normalize_sae_decoder = True,
+    from_pretrained_path = None,
+    apply_b_dec_to_input = False,
+
+    # Activation Store Parameters
+    n_batches_in_buffer = 128,
+    #training_tokens = 1_000_000_000,
+    #store_batch_size_prompts = 8,
+    #train_batch_size_tokens = batch_size,
+    #normalize_activations = (
+    #    "none"  # none, expected_average_only_in (Anthropic April Update), constant_norm_rescale (Anthropic Feb Update)
+    #),
+
+    # Misc
+    device = device,
+    seed = 42,
+    dtype = "float32",
+    prepend_bos = False,
+
+    # Training Parameters
+
+    ## Adam
+    #adam_beta1 = 0,
+    #adam_beta2 = 0.999,
+
+    ## Loss Function
+    #mse_loss_normalization = None,
+    #l1_coefficient = args.l1_coef,
+    #lp_norm = 1,
+    #scale_sparsity_penalty_by_decoder_norm = False,
+    #l1_warm_up_steps = l1_warm_up_steps,
+
+    ## Learning Rate Schedule
+    #lr = 3e-5,
+    #lr_scheduler_name = (
+    #    "constant"  # constant, cosineannealing, cosineannealingwarmrestarts
+    #),
+    #lr_warm_up_steps = lr_warm_up_steps,
+    #lr_end = None,  # only used for cosine annealing, default is lr / 10,
+    #lr_decay_steps = lr_decay_steps,
+
+    # Resampling protocol args
+    #use_ghost_grads = False,  # want to change this to true on some timeline.,
+    #feature_sampling_window = 2000,
+    #dead_feature_window = 1000,  # unless this window is larger feature sampling,,
+
+    #dead_feature_threshold = 1e-6,
+
+    # Evals
+    #n_eval_batches = 10,
+    #eval_batch_size_prompts = None,  # useful if evals cause OOM,
+
+    # WANDB
+    #log_to_wandb = False,
+    #log_activations_store_to_wandb = False,
+    #log_optimizer_state_to_wandb = False,
+    #wandb_project = "sae-transfer-learning",
+    #wandb_log_frequency = 30,
+    #eval_every_n_wandb_logs = 100,
+    #run_name = f"L{args.layer}_{args.component}_L1_{str(args.l1_coef).replace('.', '_')}",
+
+    # Misc
+    #resume = False,
+    #n_checkpoints = 10,
+    #checkpoint_path = "checkpoints",
+    #verbose = True
 )
-sparse_autoencoder.eval()
 
-# L0 test
-with torch.no_grad():
-    batch_tokens = activation_store.get_batch_tokens()
-    _, cache = model.run_with_cache(batch_tokens, prepend_bos=True)
+# Eval Config
+eval_cfg = EvalConfig(
+    batch_size_prompts = 8,
 
-    # Use the SAE
-    sae_out, feature_acts, loss, mse_loss, l1_loss, _ = sparse_autoencoder(
-        cache[sparse_autoencoder.cfg.hook_point]
+    # Reconstruction metrics
+    n_eval_reconstruction_batches = 32,
+    compute_kl = True,
+    compute_ce_loss = True,
+
+    # Sparsity and variance metrics
+    n_eval_sparsity_variance_batches = 1,
+    compute_l2_norms = True,
+    compute_sparsity_metrics = True,
+    compute_variance_metrics = False,
+)
+
+# Load SAE
+if sae_component == "RES":
+    sae_component = "rs-post"
+elif sae_component == "MLP":
+    sae_component = "mlp-out"
+elif sae_component == "ATT":
+    sae_component = "attn-z"
+
+SAE_PATH = f"/Users/ghidav/.cache/huggingface/hub/models--mech-interp--pythia-160m-deduped-{sae_component}/snapshots/3b8e8bffff1cf13322769107ecf50ceb23c406ee/L{sae_layer}"
+sae = SAE.load_from_pretrained(SAE_PATH).to(device)
+
+# Load model
+model = HookedSAETransformer.from_pretrained("pythia-160m-deduped").to(device)
+
+# Load activations store
+activations_store = ActivationsStore.from_config(
+        model,
+        cfg
     )
 
-    # save some room
-    del cache
+if __name__ == "__main__":
+    run_evals(sae, activations_store, model, eval_cfg)
 
-    # ignore the bos token, get the number of features that activated in each token, averaged accross batch and position
-    l0 = (feature_acts[:, 1:] > 0).float().sum(-1).detach()
-    print("Average l0", np.round(l0.mean().item(), 3))
-
-# Reconstruction
-def reconstr_hook(activation, hook, sae_out):
-    return sae_out
-
-def zero_abl_hook(activation, hook):
-    return torch.zeros_like(activation)
-
-print("Original: ",
-      np.round(model(
-          batch_tokens, 
-          return_type="loss"
-      ).item(), 3),
-)
-
-print(
-    "Reconstruction: ",
-    np.round(model.run_with_hooks(
-        batch_tokens,
-        fwd_hooks=[
-            (
-                sparse_autoencoder.cfg.hook_point,
-                partial(reconstr_hook, sae_out=sae_out),
-            )
-        ],
-        return_type="loss",
-    ).item(), 3),
-)
-
-print(
-    "Zero-ablation: ",
-    np.round(model.run_with_hooks(
-        batch_tokens,
-        return_type="loss",
-        fwd_hooks=[(sparse_autoencoder.cfg.hook_point, zero_abl_hook)],
-    ).item(), 3),
-)
+# python3 sae_eval.py -sae l1cRES -act l0cRES
